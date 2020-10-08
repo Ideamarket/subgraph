20ac45a4324f5e836d108fa75612104983202b50
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-10-08 11:53:10 +0200

    Update build script to create autogen files

--- build.js
+++ build.js
@@ -1,9 +1,14 @@
 const fs = require("fs")
-const { exec } = require("child_process")
+const { exec, spawn } = require("child_process")
 const path = require('path')
 
 const buildDir = './build'
 const abiDir = './abis'
+const generatedDir = './generated'
+
+const contracts = [
+    'IdeaTokenFactory'
+]
 
 async function main() {
 
@@ -56,7 +61,8 @@ async function main() {
 
     // Extracts ABIs from contract build
     console.log('> Extracting ABIs')
-    fs.readdirSync('ideamarket/build/contracts').forEach((file) => {
+    contracts.forEach((contract) => {
+        const file = contract + '.json'
         const curPath = path.join('ideamarket/build/contracts', file)
         if(!curPath.endsWith('.json')) {
             console.log('>     ignoring file ' + curPath)
@@ -68,19 +74,80 @@ async function main() {
         const abi = JSON.stringify(jsonArtifact.abi)
         fs.writeFileSync(path.join(abiDir, file), abi)
     })
+
+    // Generate autogen files
+    console.log('> Generating autogen files')
+
+    if (!fs.existsSync(generatedDir)) {
+        console.log('> Creating generated directory')
+        fs.mkdirSync(generatedDir)
+    } else {
+        console.log('> Cleaning old autogen files')
+        cleanDirectory(generatedDir)
+    }
+
+    const promises = []
+    for(let i = 0; i < contracts.length; i++) {
+        const contract = contracts[i]
+        promises.push(new Promise(async (resolve, reject) => {
+            const dir = 'autogen_' + contract
+
+            if(fs.existsSync(dir)) {
+                deleteDirectory(dir)
+            }
+
+            const abiPath = path.normalize('./abis/'+ contract + '.json')
+
+            let graphCmd = 'graph'
+            if(process.platform === "win32") {
+                graphCmd += '.cmd'
+            }
+            const child = spawn(graphCmd, ['init', '--from-contract', '0x0000000000000000000000000000000000000000', '--network', 'mainnet', '--abi', abiPath, dir + '/' + dir])
+            child.stdin.setEncoding('utf-8')
+            //child.stdout.pipe(process.stdout)
+            child.stdin.write('\n') // Name
+            await sleep(1500)
+            child.stdin.write('\n') // Directory
+            await sleep(1500)
+            child.stdin.write('\n') // Network
+            await sleep(1500)
+            child.stdin.write('0x0000000000000000000000000000000000000000\n') // Address
+            await sleep(1500)
+            child.stdin.write('\n') // ABI file path
+            child.stdin.end()
+            
+            child.on('exit', () => {
+                fs.copyFileSync(path.join(dir, 'generated/Contract/Contract.ts'), 'generated/' + contract + '.ts')
+                console.log('> Done: ' + contract)
+                resolve()
+            })
+        }))
+    }
+
+    await Promise.all(promises)
 }
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    })
+}   
 
 function cleanDirectory(dir) {
     if (fs.existsSync(dir)) {
         fs.readdirSync(dir).forEach((file) => {
-          const curPath = path.join(dir, file)
-          if (fs.lstatSync(curPath).isDirectory()) {
-            deleteFolderRecursive(curPath);
-          } else {
-            fs.unlinkSync(curPath)
-          }
+            const curPath = path.join(dir, file)
+            if (fs.lstatSync(curPath).isDirectory()) {
+                cleanDirectory(curPath);
+            } else {
+                fs.unlinkSync(curPath)
+            }
         })
     }
+}
+
+function deleteDirectory(dir) {
+    fs.rmdirSync(dir, { recursive: true })
 }
 
 async function executeCmd(cmd) {
