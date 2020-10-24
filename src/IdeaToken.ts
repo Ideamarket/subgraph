d48398ca41028e204b0a30bd9301359fb923b479
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-10-24 12:39:41 +0200

    Update for new events

--- src/IdeaToken.ts
+++ src/IdeaToken.ts
@@ -1,21 +1,27 @@
-import { Address, BigInt } from '@graphprotocol/graph-ts'
+import { Address, BigInt, BigDecimal } from '@graphprotocol/graph-ts'
 import {
 	Transfer,
 	Approval,
 	OwnershipChanged
 } from '../res/generated/IdeaToken/IdeaToken'
-import { IdeaToken, IdeaTokenBalance, IdeaTokenAllowance } from '../res/generated/schema'
+import { IdeaToken, IdeaTokenBalance, IdeaTokenAllowance, IdeaMarket, IdeaTokenPricePoint } from '../res/generated/schema'
 
 const zeroAddress = Address.fromString('0x0000000000000000000000000000000000000000')
+const tenPow18 = BigDecimal.fromString('1000000000000000000')
 
 export function handleTransfer(event: Transfer): void {
 	const token = IdeaToken.load(event.address.toHex())
 	if(!token) {
 		return
 	}
+	const market = IdeaMarket.load(token.market)
+	if(!market) {
+		throw 'Market does not exist on Transfer event'
+	}
 
 	if(event.params.from.equals(zeroAddress)) {
 		token.supply = token.supply.plus(event.params.value)
+		makePricePoint(market as IdeaMarket, token.id, token.supply, event.block.timestamp, event.block.number, event.transaction.index)
 	} else {
 		const fromBalance = IdeaTokenBalance.load(event.params.from.toHex() + '-' + token.id)
 		if (!fromBalance) {
@@ -31,6 +37,7 @@ export function handleTransfer(event: Transfer): void {
 
 	if(event.params.to.equals(zeroAddress)) {
 		token.supply = token.supply.minus(event.params.value)
+		makePricePoint(market as IdeaMarket, token.id, token.supply, event.block.timestamp, event.block.number, event.transaction.index)
 	} else {
 		let toBalance = IdeaTokenBalance.load(event.params.to.toHex() + '-' + token.id)
 		if (!toBalance) {
@@ -76,4 +83,18 @@ export function handleOwnershipChanged(event: OwnershipChanged): void {
 
 	token.owner = event.params.newOwner
 	token.save()
+}
+
+function makePricePoint(market: IdeaMarket, tokenID: string, supply: BigInt, timestamp: BigInt, block: BigInt, txindex: BigInt): void {
+	const pricePoint = new IdeaTokenPricePoint(tokenID + '-' + block.toHex() + '-' + txindex.toHex())
+	pricePoint.token = tokenID
+	pricePoint.timestamp = timestamp
+	pricePoint.block = block
+	pricePoint.txindex = txindex
+	pricePoint.price = calculateDecimalPriceFromSupply(supply, market)
+	pricePoint.save()
+}
+
+function calculateDecimalPriceFromSupply(currentSupply: BigInt, market: IdeaMarket): BigDecimal {
+	return market.baseCost.plus(currentSupply.times(market.priceRise)).toBigDecimal().div(tenPow18.times(tenPow18))
 }
--- src/IdeaTokenExchange.ts
+++ src/IdeaTokenExchange.ts
@@ -1,90 +1,88 @@
-import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
+import { BigInt, BigDecimal } from '@graphprotocol/graph-ts'
 import {
-	TokensBought,
-	TokensSold,
 	NewInterestWithdrawer,
 	NewPlatformFeeWithdrawer,
-	DaiInvested,
-	TradingFeeInvested,
-	PlatformFeeInvested,
-	OwnershipChanged
+	DaiRedeemed,
+	TradingFeeRedeemed,
+	PlatformFeeRedeemed,
+	OwnershipChanged,
+	InvestedState
 } from '../res/generated/IdeaTokenExchange/IdeaTokenExchange'
-import { IdeaToken, IdeaMarket, IdeaTokenPricePoint, IdeaTokenMarketCapPoint, IdeaTokenExchange } from '../res/generated/schema'
+import { IdeaToken, IdeaMarket, IdeaTokenMarketCapPoint, IdeaTokenExchange } from '../res/generated/schema'
 
 const tenPow18 = BigDecimal.fromString('1000000000000000000')
 
-export function handleTokensBought(event: TokensBought): void {
-
-	// Update market cap
+export function handleInvestedState(event: InvestedState): void {
+	const exchange = IdeaTokenExchange.load(event.address.toHex())
+	const market = IdeaMarket.load(event.params.marketID.toHex())
 	const token = IdeaToken.load(event.params.ideaToken.toHex())
-	if(!token) {
-		throw 'IdeaToken does not exist on TokensBought event'
+
+	if(!exchange) {
+		throw 'IdeaTokenExchange does not exist on InvestedState event'
 	}
-
-	token.marketCap = token.marketCap.plus(event.params.rawCost)
-	token.save()
-
-	// Make price and market cap point
-	const market = IdeaMarket.load(token.market)
 	if(!market) {
-		throw 'IdeaMarket does not exist on TokensBought event'
+		throw 'Market does not exist on InvestedState event'
 	}
-
-	makePricePoint(token as IdeaToken, market as IdeaMarket, event.params.finalCost, event.block.timestamp, event.block.number, event.transaction.index)
-	makeMarketCapPoint(token as IdeaToken, event.block.timestamp, event.block.number, event.transaction.index)
-}
-
-export function handleTokensSold(event: TokensSold): void {
-	const token = IdeaToken.load(event.params.ideaToken.toHex())
 	if(!token) {
-		throw 'IdeaToken does not exist on TokensSold event'
+		throw 'IdeaToken does not exist on InvestedState event'
 	}
+	
+	token.daiInToken = event.params.daiInToken
+	token.marketCap = event.params.daiInToken
+	token.invested = event.params.daiInvested
+	market.platformFeeInvested = event.params.platformFeeInvested
+	exchange.tradingFeeInvested = event.params.tradingFeeInvested
 
-	token.marketCap = token.marketCap.plus(event.params.rawPrice)
+	makeMarketCapPoint(token.id, token.marketCap, event.block.timestamp, event.block.number, event.transaction.index)
+
 	token.save()
-
-	// Make price and market cap point
-	const market = IdeaMarket.load(token.market)
-	if(!market) {
-		throw 'IdeaMarket does not exist on TokensSold event'
-	}
-
-	makePricePoint(token as IdeaToken, market as IdeaMarket, event.params.rawPrice, event.block.timestamp, event.block.number, event.transaction.index)
-	makeMarketCapPoint(token as IdeaToken, event.block.timestamp, event.block.number, event.transaction.index)
+	market.save()
+	exchange.save()
 }
 
 export function handleNewInterestWithdrawer(event: NewInterestWithdrawer): void {
 	const token = IdeaToken.load(event.params.ideaToken.toHex())
+	if(!token) {
+		throw 'IdeaToken does not exist on NewInterestWithdrawer event'
+	}
 	token.interestWithdrawer = event.params.withdrawer
 	token.save()
 }
 
 export function handleNewPlatformFeeWithdrawer(event: NewPlatformFeeWithdrawer): void {
 	const market = IdeaMarket.load(event.params.marketID.toHex())
+	if(!market) {
+		throw 'Market does not exist on handleNewPlatformFeeWithdrawer event'
+	}
 	market.platformFeeWithdrawer = event.params.withdrawer
 	market.save()
 }
 
-export function handleDaiInvested(event: DaiInvested): void {
+export function handleDaiReddemed(event: DaiRedeemed): void {
 	const token = IdeaToken.load(event.params.ideaToken.toHex())
-	token.daiInToken = event.params.daiInToken
+	if(!token) {
+		throw 'IdeaToken does not exist on DaiRedeemed event'
+	}
 	token.invested = event.params.investmentToken
 	token.save()
 }
 
-export function handlePlatformFeeInvested(event: PlatformFeeInvested): void {
+export function handlePlatformFeeRedeemed(event: PlatformFeeRedeemed): void {
 	const market = IdeaMarket.load(event.params.marketID.toHex())
-	market.platformFeeInvested = event.params.investmentToken
+	if(!market) {
+		throw 'Market does not exist on PlatformFeeRedeemed event'
+	}
+	market.platformFeeInvested = BigInt.fromI32(0)
 	market.save()
 }
 
-export function handleTradingFeeInvested(event: TradingFeeInvested): void {
+export function handleTradingFeeRedeemed(event: TradingFeeRedeemed): void {
 	const exchange = IdeaTokenExchange.load(event.address.toHex())
 	if(!exchange) {
-		throw 'IdeaTokenExchange does not exist on TradingFeeInvested event'
+		throw 'IdeaTokenExchange does not exist on TradingFeeRedeemed event'
 	}
 
-	exchange.tradingFeeInvested = event.params.investmentToken
+	exchange.tradingFeeInvested = BigInt.fromI32(0)
 	exchange.save()
 }
 
@@ -99,27 +97,12 @@ export function handleOwnershipChanged(event: OwnershipChanged): void {
 	exchange.save()
 }
 
-function makeMarketCapPoint(token: IdeaToken, timestamp: BigInt, block: BigInt, txindex: BigInt): void {
-	const marketCapPoint = new IdeaTokenMarketCapPoint(token.id + '-' + block.toHex() + '-' + txindex.toHex())
-	marketCapPoint.token = token.id
+function makeMarketCapPoint(tokenID: string, marketCap: BigInt, timestamp: BigInt, block: BigInt, txindex: BigInt): void {
+	const marketCapPoint = new IdeaTokenMarketCapPoint(tokenID + '-' + block.toHex() + '-' + txindex.toHex())
+	marketCapPoint.token = tokenID
 	marketCapPoint.timestamp = timestamp
 	marketCapPoint.block = block
 	marketCapPoint.txindex = txindex
-	marketCapPoint.marketCap = token.marketCap.toBigDecimal().div(tenPow18)
+	marketCapPoint.marketCap = marketCap.toBigDecimal().div(tenPow18)
 	marketCapPoint.save()
-}
-
-function makePricePoint(token: IdeaToken, market: IdeaMarket, volume: BigInt, timestamp: BigInt, block: BigInt, txindex: BigInt): void {
-	const pricePoint = new IdeaTokenPricePoint(token.id + '-' + block.toHex() + '-' + txindex.toHex())
-	pricePoint.token = token.id
-	pricePoint.timestamp = timestamp
-	pricePoint.block = block
-	pricePoint.txindex = txindex
-	pricePoint.price = calculateDecimalPriceFromSupply(token.supply, market)
-	pricePoint.volume = volume.toBigDecimal().div(tenPow18)
-	pricePoint.save()
-}
-
-function calculateDecimalPriceFromSupply(currentSupply: BigInt, market: IdeaMarket): BigDecimal {
-	return market.baseCost.plus(currentSupply.times(market.priceRise)).toBigDecimal().div(tenPow18)
 }
